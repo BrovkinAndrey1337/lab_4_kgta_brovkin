deadf8fbe048ca92d75b59fe88ce5326132471fe
BrovkinAndrey1337 <151190799+BrovkinAndrey1337@users.noreply.github.com>
2023-11-17 21:56:19 +0300

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Practic_1_Degtev
+{
+    public partial class Form3 : Form
+    {
+        
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Practic_1_Degtev
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+    }
+
